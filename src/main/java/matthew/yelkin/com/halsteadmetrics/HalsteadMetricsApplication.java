b18808f51436e03ef373134327a7a90807550909
MatthewYelkin <youtubetv5@mail.ru>
2024-09-15 23:40:44 +0300

    init

---
+++ src/main/java/matthew/yelkin/com/halsteadmetrics/HalsteadMetricsApplication.java
@@ -0,0 +1,12 @@
+package matthew.yelkin.com.halsteadmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalsteadMetricsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HalsteadMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matthew/yelkin/com/halsteadmetrics/HalsteadMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package matthew.yelkin.com.halsteadmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalsteadMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
